780fbf8cb98d726a27872c6ffc16cf0c91de5ae4
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-11-15 10:34:45 +0800

    1115

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/about", to: "pages#about"
+  get "/contact", to: "pages#contact"
+  # get "/home", to: "pages#home"
+
+  root"pages#home"
+  
+end
+
+
