eaf105906394b6185f5c1fcbf80f7128a798e034
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-11-17 15:22:16 +0800

    partial render

---
+++ app/controllers/wish_lists_controller.rb
@@ -0,0 +1,52 @@
+class WishListsController < ApplicationController
+  
+before_action :find_wish_list, only:[ :show ,:edit ,:update ,:destroy]
+  
+  def index
+    @wish_lists = WishList.all
+  end
+ 
+  def new
+    @wish_list = WishList.new
+  end
+
+  def create
+    @wish_list = WishList.new(clean_wish_list)
+    
+
+    if @wish_list.save
+      redirect_to root_path , notice: "許願成功～～"
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @wish_list.update(clean_wish_list)
+      redirect_to root_path , notice: "修改成功～～"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @wish_list.destroy
+    redirect_to root_path ,notice:"刪除成功"
+  end
+
+private
+  def find_wish_list
+    @wish_list = WishList.find(params[:id])
+  end
+
+  def clean_wish_list
+    params.require(:wish_list).permit( :title , :description)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
   get "/about", to: "pages#about"
   get "/contact", to: "pages#contact"
 
-  get "/make_a_wish", to: "wish_list#card"
-  get "/new_wish" , to: "wish_list#new_wish"
-  post "/catch_wish", to: "wish_list#create_wish"
-  get "/wish_card_info/:id", to: "wish_list#show_wish", as: "wish_card_info"
-  get "/wish_card_info/:id/edit", to: "wish_list#edit_wish", as: "edit_wish_card_info"
-  patch "/update_catch_wish/:id/update", to: "wish_list#update_wish", as: "update_wish_card_info"
+  resources :wish_lists
 
+  # get "/make_a_wish", to: "wish_list#card"
+  # get "/new_wish" , to: "wish_list#new"
+  # post "/catch_wish", to: "wish_list#create"
+  # get "/wish_card_info/:id", to: "wish_list#show", as: "wish_card_info"
+  # get "/wish_card_info/:id/edit", to: "wish_list#edit", as: "edit_wish_card_info"
+  # patch "/update_catch_wish/:id/update", to: "wish_list#update", as: "update_wish_card_info"
+  
+  # delete "/wish_card_info/:id", to: "wish_list#delete", as: "delete_wish_card_info"
 
-  root"wish_list#card"
+  root "wish_lists#index"
 end
