c65b62ba4a27b518ee61c66567cf87d50a924d57
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-11-27 12:35:30 +0800

    改變按鈕YES跟NO狀態

--- app/javascript/controllers/favorite-btn_controller.js
+++ app/javascript/controllers/favorite-btn_controller.js
@@ -3,14 +3,25 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "icon" ]
 
+  initialize(){
+    this.btnState = false
+  }
+
   connect() {
     console.log("favorite-btn")
-    this.iconTarget.textContent="NO"
+    
   }
 
   toggle(){
     console.log("toggle")
-    this.iconTarget.textContent="YES"
+    if (this.btnState){
+      this.btnState = true
+      this.iconTarget.textContent="NO"
+    }else{
+      this.btnState = false
+      this.iconTarget.textContent="YES"
+    }
+    this.btnState=!this.btnState
   }
 
 
